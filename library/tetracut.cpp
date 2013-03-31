bac08e2d247f21fc825e0ff83ed0d95ae1e1c2c4
lyang.thu <lyang.thu@gmail.com>
2013-03-31 17:02:12 -0400

    add new tetracut

--- library/tetracut.cpp
+++ library/tetracut.cpp
@@ -22,12 +22,11 @@ void IsoCutter::setValues(
     v4_ = v4;
 }
 
-bool IsoCutter::iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, Point &retp){
+
+bool IsoCutter::iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, Point &retp, REAL &t_par){
     //--- Check if the isodensity lies between the two value
     //printf("%f %f %f\n", v1, v2, _isoval);
     if (((v1 - _isoval) * (v2 - _isoval) <= 0) && ((v1 > v2) || (v2 > v1))) {
-        //--- Parameter
-        REAL t_par;
         //--- Get two point parametric curve of the line along the two vertices
         t_par = (_isoval - v1) / (v2 - v1);
         
@@ -38,11 +37,36 @@ bool IsoCutter::iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v
         
     }
     return false;
+    
+}
 
+bool IsoCutter::iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, Point &retp){
+    REAL t_par;
+    return iso_cut_line(_isoval, p1, p2, v1, v2, retp, t_par);
 }
 
 
-//test whether the 3 points forms 2 triangles without intersection: abc, abd 
+bool IsoCutter::iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, REAL val1, REAL val2, Point &retp, REAL &retv){
+    REAL t_par;
+    bool flag = iso_cut_line(_isoval, p1, p2, v1, v2, retp, t_par);
+    if(flag){
+        retv = (1-t_par) * val1 + t_par * val2;
+    }
+    return flag;
+    
+}
+bool IsoCutter::iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, Point &val1, Point &val2, Point &retp, Point &retv){
+    REAL t_par;
+    bool flag = iso_cut_line(_isoval, p1, p2, v1, v2, retp, t_par);
+    if(flag){
+        retv.x = (1-t_par) * val1.x  + t_par * val2.x;
+        retv.y = (1-t_par) * val1.y  + t_par * val2.y;
+        retv.z = (1-t_par) * val1.z  + t_par * val2.z;
+    }
+    return flag;
+}
+
+//test whether the 3 points forms 2 triangles without intersection: abc, abd
 //a, b, c, d must be in the same plane. Otherwise, the result won't correct
 bool IsoCutter::testTriangle(const Point &a, const Point &b, const Point &c, const Point &d){
     Point s1 = (d-a).cross(b-a);
@@ -131,8 +155,7 @@ int IsoCutter::cut(REAL isoval){
                 }
             }
         }
-        
-            
+                
         t1_.a = verts[a];
         t1_.b = verts[b];
         t1_.c = verts[c];
@@ -140,6 +163,188 @@ int IsoCutter::cut(REAL isoval){
         t2_.a = verts[a];
         t2_.b = verts[b];
         t2_.c = verts[d];
+        num_tris_ ++;
+        return num_tris_;
+    }
+    return num_tris_;
+}
+
+
+int IsoCutter::cut(REAL isoval, REAL val1, REAL val2, REAL val3, REAL val4){
+    if((isoval < v1_ && isoval < v2_ && isoval < v3_ && isoval < v4_) ||
+       (isoval > v1_ && isoval > v2_ && isoval > v3_ && isoval > v4_)){
+        return 0;
+    }
+    
+    Point verts[6];
+    REAL values[6];
+    Point zero;
+    zero.x = 0;
+    zero.y = 0;
+    zero.z = 0;
+    int vertCount = 0;
+    
+    //return 0;
+    
+    if(iso_cut_line(isoval, tetra_->v1, tetra_->v2, v1_, v2_, val1, val2, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v1, tetra_->v3, v1_, v3_, val1, val3, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v1, tetra_->v4, v1_, v4_, val1, val4, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v2, tetra_->v3, v2_, v3_, val2, val3, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v2, tetra_->v4, v2_, v4_, val2, val4, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v3, tetra_->v4, v3_, v4_, val3, val4, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    num_tris_ = 0;
+    //return num_tris_;
+    
+    if(vertCount == 3){
+        t1_.a = verts[0];
+        t1_.b = verts[1];
+        t1_.c = verts[2];
+        t1_.val1.x = values[0];
+        t1_.val2.x = values[1];
+        t1_.val3.x = values[2];
+        num_tris_ ++;
+        return num_tris_;
+    }else if(vertCount >= 4){
+        int a=0, b=1, c=2, d=3;
+        if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+            a=0; b=2; c=1; d=3;
+            if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                a=0; b=3; c=2; d=1;
+                if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                    a=1; b=2; c=0; d=3;
+                    if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                        a=1; b=3; c=0; d=2;
+                        if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                            a=2; b=3; c=1; d=0;
+                        }
+                    }
+                }
+            }
+        }
+        
+        t1_.a = verts[a];
+        t1_.b = verts[b];
+        t1_.c = verts[c];
+        t1_.val1.x = values[a];
+        t1_.val2.x = values[b];
+        t1_.val3.x = values[c];
+        num_tris_ ++;
+        
+        t2_.a = verts[a];
+        t2_.b = verts[b];
+        t2_.c = verts[d];
+        t2_.val1.x = values[a];
+        t2_.val2.x = values[b];
+        t2_.val3.x = values[d];
+        num_tris_ ++;
+        return num_tris_;
+    }
+    return num_tris_;
+}
+
+
+int IsoCutter::cut(REAL isoval, Point &val1, Point &val2, Point &val3, Point &val4){
+    if((isoval < v1_ && isoval < v2_ && isoval < v3_ && isoval < v4_) ||
+       (isoval > v1_ && isoval > v2_ && isoval > v3_ && isoval > v4_)){
+        return 0;
+    }
+    
+    Point verts[6];
+    Point values[6];
+    Point zero;
+    zero.x = 0;
+    zero.y = 0;
+    zero.z = 0;
+    int vertCount = 0;
+    
+    //return 0;
+    
+    if(iso_cut_line(isoval, tetra_->v1, tetra_->v2, v1_, v2_, val1, val2, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v1, tetra_->v3, v1_, v3_, val1, val3, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v1, tetra_->v4, v1_, v4_, val1, val4, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v2, tetra_->v3, v2_, v3_, val2, val3, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v2, tetra_->v4, v2_, v4_, val2, val4, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    if(iso_cut_line(isoval, tetra_->v3, tetra_->v4, v3_, v4_, val3, val4, verts[vertCount], values[vertCount])){
+        vertCount ++;
+    }
+    
+    num_tris_ = 0;
+    //return num_tris_;
+    
+    if(vertCount == 3){
+        t1_.a = verts[0];
+        t1_.b = verts[1];
+        t1_.c = verts[2];
+        t1_.val1 = values[0];
+        t1_.val2 = values[1];
+        t1_.val3 = values[2];
+        num_tris_ ++;
+        return num_tris_;
+    }else if(vertCount >= 4){
+        int a=0, b=1, c=2, d=3;
+        if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+            a=0; b=2; c=1; d=3;
+            if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                a=0; b=3; c=2; d=1;
+                if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                    a=1; b=2; c=0; d=3;
+                    if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                        a=1; b=3; c=0; d=2;
+                        if(!testTriangle(verts[a], verts[b], verts[c], verts[d])){
+                            a=2; b=3; c=1; d=0;
+                        }
+                    }
+                }
+            }
+        }
+        
+        t1_.a = verts[a];
+        t1_.b = verts[b];
+        t1_.c = verts[c];
+        t1_.val1 = values[a];
+        t1_.val2 = values[b];
+        t1_.val3 = values[c];
+        num_tris_ ++;
+        
+        t2_.a = verts[a];
+        t2_.b = verts[b];
+        t2_.c = verts[d];
+        t2_.val1 = values[a];
+        t2_.val2 = values[b];
+        t2_.val3 = values[d];
         num_tris_ ++;
         return num_tris_;
     }
--- library/tetracut.h
+++ library/tetracut.h
@@ -14,6 +14,11 @@ public:
     //return how many triangles are there after this cutting 
     int cut(REAL isovalue);
     
+    //cut the tetrahedron with value interpolated to the value of the triangle
+    int cut(REAL isovalue, REAL val1, REAL val2, REAL val3, REAL val4);
+    int cut(REAL isovalue, Point &val1, Point &val2, Point &val3, Point &val4);
+    
+    
     //get the i-th triangles after the cut
     Triangle3d& getTrangle(int i);
 
@@ -22,12 +27,18 @@ public:
     //cut the line between p1, p2 with value v1, v2. Returns the point in retp
     //if no intersection, return false, else return true
     bool iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, Point &retp);
+    bool iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, REAL val1, REAL val2, Point &retp, REAL &retv);
+    bool iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, Point &val1, Point &val2, Point &retp, Point &retv);
+    
+    
 private:
     Tetrahedron *tetra_;
     REAL v1_, v2_, v3_, v4_;
     Triangle3d t1_, t2_;
     int num_tris_;
 
+    
+    bool iso_cut_line(REAL _isoval, Point &p1, Point &p2, REAL v1, REAL v2, Point &retp, REAL &t_par);
      
 };
 #endif
