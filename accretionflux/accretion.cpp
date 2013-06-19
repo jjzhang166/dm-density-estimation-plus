6ee6080e714b096ac07cebc95c3de17f800678af
lyang.thu <lyang.thu@gmail.com>
2013-06-18 23:42:28 -0400

    fix accretion

--- accretionflux/accretion.cpp
+++ accretionflux/accretion.cpp
@@ -8,6 +8,7 @@ using namespace std;
 double accretion_sphere_rate(int counts, Point * posdata, Point * veldata, double mass, Point &halocenter, double r1, double r2){
     double volum = 4.0 * PI / 3.0 * (r2 * r2 * r2 - r1 * r1 * r1);
     double accr = 0.0;
+    double dr = abs(r2 - r1);
     //printf("Points %d %f\n", counts, 1.0/volum);
     for(int i = 0; i < counts; i ++){
         //flag the particles
@@ -21,7 +22,7 @@ double accretion_sphere_rate(int counts, Point * posdata, Point * veldata, doubl
             accr += rvec.dot(veldata[i]);
         }
     }
-    return accr * mass / volum * 4.0 * PI * r1 * r1;
+    return accr * mass / dr;// * 4.0 * PI * r1 * r1 / volum;
 }
 
 double getRadius(const Point &v1, const Point &v2){
@@ -68,21 +69,28 @@ double accretion_tetra_rate(
             for(int j = 0; j < nu_tri; j++){
                 Triangle3d t = cutter.getTrangle(j);
                 Point direc = halocenter - (t.a + t.b + t.c) / 3.0 ;
+                
+                double rp = sqrt(direc.dot(direc));
+                
                 //printf("%f\n", sqrt(direc.dot(direc)));
-                direc = direc / sqrt(direc.dot(direc));
+                direc = direc / rp;
                 
                 double velocity = direc.dot(t.val1 + t.val2 + t.val3) / 3.0;
-                double area = t.getArea();
+                //printf("%e\n", velocity);
+                double area = t.getArea();// / (r*r);
                 //printf("\n");
                 /*printf("%f %f %f\n", t.a.x, t.a.y, t.a.z);
                 printf("%f %f %f\n", t.b.x, t.b.y, t.b.z);
                 printf("%f %f %f\n", t.c.x, t.c.y, t.c.z);
                 
+               
+                printf("r=%f, dis=%f, ratio=%f\n", r, rp, rp/r);
+                
                 printf("%f %f %f\n", t.val1.x, t.val1.y, t.val1.z);
                 printf("%f %f %f\n", t.val2.x, t.val2.y, t.val2.z);
                 printf("%f %f %f\n", t.val3.x, t.val3.y, t.val3.z);*/
 
-                accr += area * velocity * mass * tetras[i].invVolume;
+                accr += area * velocity * mass * tetras[i].invVolume / 6.0;
                 //printf("Area: %f %f\n", area, area/(4*PI*r*r));
                 //printf("\n");
                 
--- accretionflux/main.cpp
+++ accretionflux/main.cpp
@@ -3,11 +3,12 @@
 
 using namespace std;
 
-string filename_ =  "/Users/lyang/data/run_200";
+
 //string filename_ =  "/Users/lyang/data/run_200";
+//string filename_ =  "/Users/lyang/data/32Mpc_S1_PM_000";
 Point halocenter;
-double r = 1110;
-double dr = 50;
+double r = 10000;
+double dr = 100;
 
 //load all the particles into memory?
 bool isHighMem = true;
@@ -19,53 +20,39 @@ bool isAllData = false;
 int datagridsize = 256;
 //the particle type in the gadget file
 int parttype = 1;
-int inputmemgrid = 16;
+int inputmemgrid = 32;
 
 bool isInOrder = false;
 bool isVelocity = true;
 
 int main(){
+    
+    string filename_ =  "/Users/lyang/data/trygad.data";
+    
     GSnap * gsnap_ = new GSnap(filename_, isHighMem, parttype, datagridsize);
     int numparts = gsnap_->Npart;
     double mass = gsnap_->header.mass[1];
-    printf("Particle Numbers: %d\n", numparts);
-    printf("Particle mass: %f\n", mass);
-    //fstream file(filename_.c_str(), ios_base::in | ios_base::binary);
-    //if(!file.good()){
-    //    printf("Data file bad!\n");
-    //    exit(1);
-    //}
 
     Point * pos = gsnap_ -> getAllPos();//new Point[numparts];
     Point * vel = gsnap_ -> getAllVel();//new Point[numparts];
-
-    //gsnap_->readPos(file, pos, 0, numparts);
-    //gsnap_->readVel(file, vel, 0, numparts);
    
-    //for(int i = 0; i < numparts; i++){
-    //    printf("Pos: %f %f %f\n", pos[i].x, pos[i].y, pos[i].z);
-    //    printf("Vel: %f %f %f\n", vel[i].x, vel[i].y, vel[i].z);
-    //}
+    /*for(int i = 0; i < numparts; i++){
+        printf("Pos: %f %f %f\n", pos[i].x, pos[i].y, pos[i].z);
+        printf("Vel: %f %f %f\n", vel[i].x, vel[i].y, vel[i].z);
+    }*/
 
 
 
-    halocenter.x = 28464.0;
-    halocenter.y = 15309.0;
-    halocenter.z = 25433.7;
+    halocenter.x = 16000;
+    halocenter.y = 16000.0;
+    halocenter.z = 16000.0;
     
 
-    double ar_sphere = accretion_sphere_rate(
-                    numparts, 
-                    pos, 
-                    vel, 
-                    mass, 
-                    halocenter,
-                    r, 
-                    r+dr);
+    double ar_sphere;
     
-    printf("Measure in radius %f, with thickness %f. Accretion rate is %e\n",
-           r, dr, ar_sphere);
-    std::cout.flush();
+    //printf("Measure in radius %f, with thickness %f. Accretion rate is %e\n",
+    //       r, dr, ar_sphere);
+    //std::cout.flush();
     
     TetraStreamer streamer(filename_,
                            inputmemgrid,
@@ -77,20 +64,129 @@ int main(){
                            true,
                            isInOrder);
     
-    double ar_tetra = accretion_tetra_rate( 
+    double ar_tetra;/* = accretion_tetra_rate(
                     streamer,  
                     mass, 
                     halocenter,
-                    r);
+                    r);*/
 
 
-    printf("Measure in LTFE %f. Area is %f. Accretion rate is %e\n", 
-                    r, 4*PI*r*r, ar_tetra);
+    //printf("Measure in LTFE %f. Area is %e. Accretion rate is %e\n",
+    //               r, 4*PI*r*r, ar_tetra);
+    
+    
+    //test
+    double rho = numparts * mass /
+        ((gsnap_->header.BoxSize)*(gsnap_->header.BoxSize)*(gsnap_->header.BoxSize));
+    //printf("Test:\n, Calculated rate is rho * v * 4 pi r^2 = %e\n", - rho * r * 4 * 3.1415926 * r * r); //* (1/2.0 + 1/2.0 / 10.0));
 
+    
+    dr = 10;
+    printf("Artifitial data: \n");
+    
+    printf("Particle Numbers: %d\n", numparts);
+    printf("Particle mass: %f\n", mass);
 
-    //file.close();
+    
+    printf("r, Accurate, LTFE, Sphere ");
+    for(int i = 1; i <= 20; i +=2){
+        printf("dr = %3.0f ", dr * i);
+    }
+    printf("\n");
+    /*for(r = 100.0; r < 20000; r+=1000){
+        double accurate_accr =  - rho * r * 4 * 3.1415926 * r * r;
+        
+        streamer.reset();
+        ar_tetra = accretion_tetra_rate(
+                  streamer,
+                  mass,
+                  halocenter,
+                  r);
+        
+        
+        printf("%f %e %e ", r, accurate_accr, ar_tetra);
+        for(int i = 1; i <= 20; i +=2){
+            ar_sphere = accretion_sphere_rate(
+                                              numparts,
+                                              pos,
+                                              vel,
+                                              mass,
+                                              halocenter,
+                                              r,
+                                              r + dr * i);
+            printf(" %e ", ar_sphere);
+            cout.flush();
+        }
+        printf("\n");
+
+    }*/
+    
     
     delete gsnap_;
+    
+    
+    printf("Real data: \n");
+    filename_ = "/Users/lyang/data/run_200";
+    
+    
+    halocenter.x = 28464.0;
+    halocenter.y = 15309.0;
+    halocenter.z = 25433.7;
+    
+    
+    GSnap * gsnap1_ = new GSnap(filename_, isHighMem, parttype, datagridsize);
+    numparts = gsnap1_->Npart;
+    mass = gsnap1_->header.mass[1];
+    printf("Particle Numbers: %d\n", numparts);
+    printf("Particle mass: %f\n", mass);
+    
+    pos = gsnap1_ -> getAllPos();//new Point[numparts];
+    vel = gsnap1_ -> getAllVel();//new Point[numparts];
+    
+    TetraStreamer streamer1(filename_,
+                           inputmemgrid,
+                           parttype,
+                           datagridsize,
+                           isHighMem,
+                           isAllData,
+                           isVelocity,
+                           true,
+                           isInOrder);
+    
+    
+    
+    printf("r, Accurate, LTFE, Sphere ");
+    for(int i = 1; i <= 20; i +=2){
+        printf("dr = %3.0f ", dr * i);
+    }
+    printf("\n");
+    for(r = 5500; r < 6500; r+=100){
+        
+        streamer1.reset();
+        ar_tetra = accretion_tetra_rate(
+                                        streamer1,
+                                        mass,
+                                        halocenter,
+                                        r);
+        
+        printf("%f %e ", r, ar_tetra);
+        for(int i = 1; i <= 20; i +=2){
+            ar_sphere = accretion_sphere_rate(
+                                              numparts,
+                                              pos,
+                                              vel,
+                                              mass,
+                                              halocenter,
+                                              r,
+                                              r + dr * i);
+            printf(" %e ", ar_sphere);
+            cout.flush();
+        }
+        printf("\n");
+        
+    }
+    
+    delete gsnap1_;
     
     return 0;
 }
