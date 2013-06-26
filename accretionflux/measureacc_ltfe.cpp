c147ad8c46fb251ada7bc224ae40eb09cfa1fd78
lyang.thu <lyang.thu@gmail.com>
2013-06-26 02:52:23 -0400

    add mask file prop to ltfe measurement

--- accretionflux/measureacc_ltfe.cpp
+++ accretionflux/measureacc_ltfe.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>     /* atoi */
 #include <fitsio.h>
 #include <sstream>
+#include <fstream>
 #include <vector>
 #include <iostream>
 #include "accretion.h"
@@ -15,39 +16,54 @@ void printusage(){
     printf("Usage: measureacc_ltfe datafile halocatalog\n");
     printf("[-radius <the radius measured in r_vir>]\n");
     printf("[-halo <{ list of halos to measure }>]\n");
+    printf("[-mask <mask file for the halo >]\n");
+    printf("[-massthd <mass threshold for halos, under which to be calculated (-1 default)>]\n");
     printf("\n");
     printf("Measure the accretion rate of halos in the datafile (gadget file)\n descripted by the halocatalog (fits file)\n");
     printf("The default radius is 1.5\n");
 }
 
-//string filename_ =  "/Users/lyang/data/32Mpc_S1_PM_000";
-Point halocenter;
-double r = 10000;
-double dr = 100;
-
-//load all the particles into memory?
-bool isHighMem = true;
-//return the all particle pointer?
-bool isAllData = false;
-
-//if use -1, then use the particle gridsize as the gridsize
-//otherwise use the user setting
-int datagridsize = 256;
-//the particle type in the gadget file
-int parttype = 1;
-int inputmemgrid = 32;
-
-bool isInOrder = false;
-bool isVelocity = true;
 
 
 int main(int argc, char *argv[])
 {
     string datafile;
     string halofile;
+    string maskfile = "";
+    
     double radtimes = 1.5;
+    double massthred = -1.0;
+    
     std::vector<int> halovec;
     bool isUseHaloVec = false;
+    
+    
+    //string filename_ =  "/Users/lyang/data/32Mpc_S1_PM_000";
+    Point halocenter;
+    double r = 10000;
+    double dr = 100;
+    
+    //load all the particles into memory?
+    bool isHighMem = true;
+    //return the all particle pointer?
+    bool isAllData = false;
+    
+    //if use -1, then use the particle gridsize as the gridsize
+    //otherwise use the user setting
+    int datagridsize = 256;
+    //the particle type in the gadget file
+    int parttype = 1;
+    int inputmemgrid = 32;
+    
+    bool isInOrder = false;
+    bool isVelocity = true;
+    
+    char * halomask;
+    fstream maskstream;
+    
+    
+    
+
     
     if (argc < 3) {
         printusage();
@@ -89,7 +105,17 @@ int main(int argc, char *argv[])
                     }
                 }
                 
-            }else{
+            }else if(strcmp(argv[k], "-mask") == 0){
+				ss << argv[k + 1];
+				k +=2;
+                ss >> maskfile;
+                //printf("%f\n", radtimes);
+			}else if(strcmp(argv[k], "-massthd") == 0){
+				ss << argv[k + 1];
+				k +=2;
+                ss >> massthred;
+                //printf("%f\n", radtimes);
+			}else{
                 printusage();
                 return 1;
             }
@@ -99,6 +125,31 @@ int main(int argc, char *argv[])
     
     int hmin =0;
     int hmax = getTotalHaloNum(halofile.c_str());
+    halomask = new char[hmax];
+    for(int i = 0; i < hmax; i++){
+        halomask[i] = 1;
+    }
+    
+    
+    if(maskfile != ""){
+        maskstream.open(maskfile.c_str(), ios::in | ios::binary);
+        if(maskstream.good()){
+            int head[64];
+            maskstream.read((char *) head, sizeof(int) * 64);
+            if(head[0] != hmax){
+                printf("Number of halos in halo mask file doesnot match! \n");
+                maskstream.close();
+                exit(1);
+            }else{
+                maskstream.read(halomask, hmax * sizeof(char));
+            }
+        }else{
+            printf("Mask file corrupted!\n");
+            exit(1);
+        }
+    }
+    maskstream.close();
+    
     if(isUseHaloVec){
         hmin = 0;
         hmax = halovec.size();
@@ -124,6 +175,12 @@ int main(int argc, char *argv[])
         }else{
             haloid = i + 1;
         }
+        
+        
+        if(halomask[haloid] == 0){
+            continue;
+        }
+        
         int status = getHaloById(halofile.c_str(), haloid, &halo);
         if(status != 0){
             break;
@@ -145,4 +202,5 @@ int main(int argc, char *argv[])
         cout.flush();
     }
     
+    delete halomask;
 }
