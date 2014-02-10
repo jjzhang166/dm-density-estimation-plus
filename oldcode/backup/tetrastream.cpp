6844dddc5ba95bdc435e48b0269df7b6a8c387e5
lyang.thu <lyang.thu@gmail.com>
2014-02-10 18:03:43 -0500

    move old code to oldcode

---
+++ oldcode/backup/tetrastream.cpp
@@ -0,0 +1,375 @@
+/*
+ * tetrastream.cpp
+ *
+ *  Created on: Dec 18, 2012
+ *      Author: lyang
+ */
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#include "tetrahedron.h"
+#include "tetrastream.h"
+#include "readgadget.h"
+
+//inputmemgridsize should be a divisor of the total_grid_size
+TetraStream::TetraStream(string filename, int inputmemgridsize, bool isVelocity) {
+	isVelocity_ = isVelocity;
+
+	filename_ = filename;
+
+	gsnap_ = new GSnap(filename_);
+	particle_grid_size_ = (int)ceil(pow(gsnap_->Npart, 1.0 / 3.0));
+	total_parts_ = gsnap_->Npart;
+	current_tetra_num = 0;
+
+	current_ind_tetra = 0;
+	current_ind_block = 0;
+    
+    printf("Particle Data Grid Size %d\n", particle_grid_size_);
+
+    mem_grid_size_ = inputmemgridsize;
+	mem_tetra_size_ = 6 * (mem_grid_size_) * (mem_grid_size_)
+        * (mem_grid_size_) * 8; //8 is for the periodical condition
+
+    
+	tetras_ = new Tetrahedron[mem_tetra_size_];
+	position_ = new Point[(mem_grid_size_ + 1) * (mem_grid_size_ + 1) * (mem_grid_size_ + 1)];
+	if(isVelocity_){
+		velocity_ = new Point[(mem_grid_size_ + 1) * (mem_grid_size_ + 1) * (mem_grid_size_ + 1)];
+	}
+
+	for(int i = 0; i < (mem_grid_size_ + 1) * (mem_grid_size_ + 1) * (mem_grid_size_ + 1); i++){
+		position_[i].x = position_[i].y = position_[i].z = 0.0;
+		if(isVelocity_){
+			velocity_[i].x = velocity_[i].y = velocity_[i].z = 0.0;
+		}
+	}
+
+	total_tetra_grid_num_ = particle_grid_size_ / mem_grid_size_ *
+			particle_grid_size_ / mem_grid_size_ *
+			particle_grid_size_ / mem_grid_size_;
+
+	//grids_ = NULL;
+
+	isPeriodical_ = false;
+	isInOrder_ = false;
+
+}
+
+void TetraStream::setIsInOrder(bool isinorder){
+	isInOrder_ = isinorder;
+}
+
+bool TetraStream::reset() {
+	loadBlock(0);
+	return true;
+}
+
+TetraStream::~TetraStream() {
+	delete position_;
+	delete gsnap_;
+	delete tetras_;
+	if(isVelocity_){
+		delete velocity_;
+	}
+}
+
+int TetraStream::getTotalBlockNum(){
+	return total_tetra_grid_num_;
+}
+
+int TetraStream::getBlockSize(){
+	return mem_grid_size_;
+}
+
+int TetraStream::getBlockNumTetra(){
+	return current_tetra_num;
+}
+
+Tetrahedron * TetraStream::getCurrentBlock(){
+	return tetras_;
+}
+
+Tetrahedron * TetraStream::getBlock(int i){
+	loadBlock(i);
+	return tetras_;
+}
+
+int TetraStream::getCurrentInd(){
+	return current_ind_block;
+}
+
+void TetraStream::loadBlock(int i){
+	if(i >= this->total_tetra_grid_num_){
+		return;
+	}
+	int imin, jmin, kmin, imax, jmax, kmax;
+	int ngb = particle_grid_size_ / mem_grid_size_;
+	imin = i % ngb * mem_grid_size_;
+	jmin = i / ngb % ngb * mem_grid_size_;
+	kmin = i / ngb / ngb % ngb * mem_grid_size_;
+	imax = imin + mem_grid_size_;
+
+	//periodical condition
+	if(imax == particle_grid_size_){
+		//imax = particle_grid_size_ - 1;
+	}
+	jmax = jmin + mem_grid_size_;
+	if(jmax == particle_grid_size_){
+		//jmax = particle_grid_size_ - 1;
+	}
+	kmax = kmin + mem_grid_size_;
+	if(kmax == particle_grid_size_){
+		//kmax = particle_grid_size_ - 1;
+	}
+
+	if(!isVelocity_){
+		gsnap_->readPosBlock(position_, imin, jmin, kmin, imax, jmax, kmax, isPeriodical_, isInOrder_);
+	}else{
+		gsnap_->readBlock(position_, velocity_, imin, jmin, kmin, imax, jmax, kmax, isPeriodical_, isInOrder_); 
+	}
+
+	//for(int ffi =0; ffi < ((imax - imin + 1)*(jmax - jmin + 1)*(kmax - kmin + 1)); ffi ++){
+	//	printf("---%e\n", position_[ffi].x);
+	//}
+
+
+	convertToTetrahedron(imax - imin + 1, jmax - jmin + 1, kmax - kmin + 1);
+	current_ind_block = i;
+}
+
+void TetraStream::splitTetraX(Tetrahedron & tetra, Tetrahedron & tetra1){
+	static Point * vertexs[4];
+	static Point * temp_;
+	vertexs[0] = &(tetra.v1);
+	vertexs[1] = &(tetra.v2);
+	vertexs[2] = &(tetra.v3);
+	vertexs[3] = &(tetra.v4);
+	for(int i = 0; i < 4; i ++){
+		for(int j = i+1; j < 4; j++){
+			if((vertexs[i]->x) < (vertexs[j]->x)){
+				temp_ = vertexs[j];
+				vertexs[j] = vertexs[i];
+				vertexs[i] = temp_;
+			}
+		}
+	}
+	vertexs[3]->x += box;
+	if(vertexs[3]->x - vertexs[2]->x > box / 2.0){
+		vertexs[2]->x += box;
+		if(vertexs[2]->x - vertexs[1]->x > box / 2.0){
+			vertexs[1]->x += box;
+		}
+	}
+	tetra.computeVolume();
+
+	tetra1 = tetra;
+	tetra1.v1.x -= box;
+	tetra1.v2.x -= box;
+	tetra1.v3.x -= box;
+	tetra1.v4.x -= box;
+	tetra1.computeMaxMin();
+
+}
+
+void TetraStream::splitTetraY(Tetrahedron & tetra, Tetrahedron & tetra1){
+	static Point * vertexs[4];
+	static Point * temp_;
+	vertexs[0] = &(tetra.v1);
+	vertexs[1] = &(tetra.v2);
+	vertexs[2] = &(tetra.v3);
+	vertexs[3] = &(tetra.v4);
+	for(int i = 0; i < 4; i ++){
+		for(int j = i+1; j < 4; j++){
+			if((vertexs[i]->y) < (vertexs[j]->y)){
+				temp_ = vertexs[j];
+				vertexs[j] = vertexs[i];
+				vertexs[i] = temp_;
+			}
+		}
+	}
+	vertexs[3]->y += box;
+	if(vertexs[3]->y - vertexs[2]->y > box / 2.0){
+		vertexs[2]->y += box;
+		if(vertexs[2]->y - vertexs[1]->y > box / 2.0){
+			vertexs[1]->y += box;
+		}
+	}
+	tetra.computeVolume();
+
+	tetra1 = tetra;
+	tetra1.v1.y -= box;
+	tetra1.v2.y -= box;
+	tetra1.v3.y -= box;
+	tetra1.v4.y -= box;
+	tetra1.computeMaxMin();
+
+}
+
+void TetraStream::splitTetraZ(Tetrahedron & tetra, Tetrahedron & tetra1){
+	static Point * vertexs[4];
+	static Point * temp_;
+	vertexs[0] = &(tetra.v1);
+	vertexs[1] = &(tetra.v2);
+	vertexs[2] = &(tetra.v3);
+	vertexs[3] = &(tetra.v4);
+	for(int i = 0; i < 4; i ++){
+		for(int j = i+1; j < 4; j++){
+			if((vertexs[i]->z) < (vertexs[j]->z)){
+				temp_ = vertexs[j];
+				vertexs[j] = vertexs[i];
+				vertexs[i] = temp_;
+			}
+		}
+	}
+	vertexs[3]->z += box;
+	if(vertexs[3]->z - vertexs[2]->z > box / 2.0){
+		vertexs[2]->z += box;
+		if(vertexs[2]->z - vertexs[1]->z > box / 2.0){
+			vertexs[1]->z += box;
+		}
+	}
+	tetra.computeVolume();
+
+	tetra1 = tetra;
+	tetra1.v1.z -= box;
+	tetra1.v2.z -= box;
+	tetra1.v3.z -= box;
+	tetra1.v4.z -= box;
+	tetra1.computeMaxMin();
+
+}
+
+void TetraStream::addTetra(int ind1, int ind2, int ind3, int ind4) {
+	static Tetrahedron tetras_p[8];	//for periodical correction
+	Tetrahedron &tetra_ = tetras_p[0];
+	tetra_.v1 = position_[ind1];
+	tetra_.v2 = position_[ind2];
+	tetra_.v3 = position_[ind3];
+	tetra_.v4 = position_[ind4];
+
+	if(isVelocity_){
+		tetra_.velocity1 = velocity_[ind1];
+		tetra_.velocity2 = velocity_[ind2];
+		tetra_.velocity3 = velocity_[ind3];
+		tetra_.velocity4 = velocity_[ind4];
+	}
+	tetra_.computeVolume();
+
+	//add the tetrahedrons
+	/*if(grids_ == NULL){
+		tetras_[current_ind_tetra] = (tetra_);
+		current_ind_tetra ++;
+	}
+	//correction
+	else*/
+    if(isPeriodical_){
+		//periodical correction:
+		int tetra_num = 1;
+		int temp_num = 0;
+		if(tetra_.maxx() - tetra_.minx() > box / 2.0){
+			splitTetraX(tetra_, tetras_p[1]);
+			tetra_num ++;
+		}
+		temp_num = 0;
+		for(int i = 0; i < tetra_num; i++){
+			Tetrahedron &t = tetras_p[i];
+			if(t.maxy() - t.miny() > box / 2.0){
+				splitTetraY(t, tetras_p[tetra_num + temp_num]);
+				temp_num ++;
+			}
+		}
+		tetra_num += temp_num;
+		temp_num = 0;
+		for(int i = 0; i < tetra_num; i++){
+			Tetrahedron &t = tetras_p[i];
+			if(t.maxz() - t.minz() > box / 2.0){
+				splitTetraZ(t, tetras_p[tetra_num + temp_num]);
+				temp_num ++;
+			}
+		}
+		tetra_num += temp_num;
+
+        
+        /*int outputGridSize = grids_->getGridSize();
+        double outputBoxSize = grids_->getEndPoint().x - grids_->getStartPoint().x;*/
+		for(int i = 0; i < tetra_num; i++){
+			Tetrahedron &t = tetras_p[i];
+			//single vox_vol
+			/*int xindmin = (int)((t.minx() - grids_->getStartPoint().x) / outputBoxSize * outputGridSize);
+			int xindmax = (int)((t.maxx() - grids_->getStartPoint().x) / outputBoxSize * outputGridSize);
+			int yindmin = (int)((t.miny() - grids_->getStartPoint().y) / outputBoxSize * outputGridSize);
+			int yindmax = (int)((t.maxy() - grids_->getStartPoint().y) / outputBoxSize * outputGridSize);
+			int zindmin = (int)((t.minz() - grids_->getStartPoint().z) / outputBoxSize * outputGridSize);
+			int zindmax = (int)((t.maxz() - grids_->getStartPoint().z) / outputBoxSize * outputGridSize);
+			int n_samples = (xindmax - xindmin + 1) * (yindmax - yindmin + 1) * (zindmax - zindmin + 1);
+			if(n_samples == 1){
+				grids_->setValueByActualCoor(xindmin, yindmin, zindmin, 6.0f / vox_vol);
+			}else*/{
+				tetras_[current_ind_tetra] = t;
+				current_ind_tetra ++;
+				//printf("%d\n", current_ind_tetra);
+			}
+		}
+		
+	}
+}
+
+void TetraStream::addTetra(int i1, int j1, int k1, int i2, int j2, int k2,
+		int i3, int j3, int k3, int i4, int j4, int k4,
+		int isize, int jsize, int ksize) {// add a tetra to the vectot
+	int ind1, ind2, ind3, ind4;
+	ind1 = (i1) + (j1) * isize + (k1) * isize * jsize;
+	ind2 = (i2) + (j2) * isize + (k2) * isize * jsize;
+	ind3 = (i3) + (j3) * isize + (k3) * isize * jsize;
+	ind4 = (i4) + (j4) * isize + (k4) * isize * jsize;
+	addTetra(ind1, ind2, ind3, ind4);
+}
+
+void TetraStream::convertToTetrahedron(int ii, int jj, int kk) {
+	current_ind_tetra = 0;
+	int i, j, k;		//loop variables
+
+	for (k = 0; k < kk-1; k++) {
+		for (j = 0; j < jj-1; j++) {
+			for (i = 0; i < ii-1; i++) {
+				//1
+				addTetra(i, j, k, i, j + 1, k, i, j, k + 1, i + 1, j, k + 1,
+						ii, jj, kk);
+				//2
+				addTetra(i, j, k, i, j + 1, k, i + 1, j, k + 1, i + 1, j, k,
+						ii, jj, kk);
+				//3
+				addTetra(i, j, k + 1, i, j + 1, k + 1, i + 1, j, k + 1, i,
+						j + 1, k, ii, jj, kk);
+				//4
+				addTetra(i, j + 1, k, i + 1, j, k + 1, i + 1, j + 1, k + 1, i,
+						j + 1, k + 1, ii, jj, kk);
+				//5
+				addTetra(i, j + 1, k, i + 1, j, k + 1, i + 1, j + 1, k + 1,
+						i + 1, j + 1, k, ii, jj, kk);
+				//6
+				addTetra(i, j + 1, k, i + 1, j, k + 1, i + 1, j + 1, k, i + 1,
+						j, k, ii, jj, kk);
+
+			}
+		}
+	}
+	current_tetra_num = current_ind_tetra;
+}
+
+void TetraStream::setCorrection(/*GridManager * grid*/){
+	//this->grids_ = grid;
+	//double boxOfGrids = (REAL)(grids_->getEndPoint().x - grids_->getStartPoint().x);
+    box = getHeader().BoxSize;
+	//ng = (REAL)grids_->getGridSize();
+	//vox_vol = boxOfGrids * boxOfGrids * boxOfGrids / ng / ng / ng;
+	isPeriodical_ = true;
+}
+
+gadget_header TetraStream::getHeader(){
+	return this->gsnap_->header;	//get the header
+}
---
+++ oldcode/velmeasure/velzdisp.cpp
@@ -0,0 +1,292 @@
+//#define __OMP__
+
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <stdint.h>
+#include <vector>
+#include <map>
+#include <set>
+#include <cfloat>
+
+#include <kdtree++/kdtree.hpp>
+
+#include "readgadget.h"
+
+#define FIND_EPSILON FLT_EPSILON
+
+
+//#define TREE_CODE
+
+using namespace std;
+
+
+int num_of_bins = 2000;
+double bin_size = 1.0;
+double max_w = 1000.0;
+
+
+#define SQRT3 1.73205080757
+
+//using namespace std;
+
+string singlefilename = "";
+string prefix = "";
+string basename_ = "";
+int numOfFiles = 0;
+float radius = 1000.0;
+float boxSize = 100.0;
+Point wvector;
+Point rvector;
+uint32_t * z_w;
+
+
+Point * allpos;
+Point * allvel;
+
+
+struct kdtreeNode
+{
+    typedef REAL value_type;
+    
+    size_t index;
+    
+    value_type operator[](size_t n) const
+    {
+        switch (n) {
+            case 0:
+                return allpos[index].x;
+            case 1:
+                return allpos[index].y;
+            case 2:
+                return allpos[index].z;
+            default:
+                return allpos[index].x;
+        }
+        return allpos[index].x;
+    }
+    
+};
+
+typedef KDTree::KDTree<3,kdtreeNode> treeType;
+treeType kdtree;
+
+
+vector<kdtreeNode> retVec;
+
+//find the particles in between 2 cube shells
+//r1 and r2 are the 1/2 of the edge length of the 2 cube
+//point p is the center of the 2 cubes
+//r1 < r2
+void findPartsInBox(treeType &tree, Point & p,
+                      Point vecP, REAL r,
+                      vector<kdtreeNode> & retVec){
+    kdtreeNode tempnod;
+    tempnod.index = 0;
+    
+    treeType::_Region_ * region = new treeType::_Region_(tempnod);
+    
+    
+    Point finalP = p + vecP;
+    //up
+    region->_M_low_bounds[0] = finalP.x - r;
+    region->_M_high_bounds[0] = finalP.x + r;
+    
+    region->_M_low_bounds[1] = finalP.y - r;
+    region->_M_high_bounds[1] = finalP.y + r;
+    
+    region->_M_low_bounds[2] = finalP.z - r;
+    region->_M_high_bounds[2] = finalP.z + r;
+    
+    
+    tree.find_within_range(*region,
+                           back_insert_iterator<vector<kdtreeNode> >(retVec));
+    
+    
+}
+
+void printUsage(string pname){
+    fprintf(stderr, "Usage: %s <Single Filename> <radius> <boxSize>\n",
+            pname.c_str()
+            );
+    fprintf(stderr, "-OR-\n");
+    fprintf(stderr, "%s <prefix> <basename> <numOfFiles> <radius> <boxSize>\n",
+            pname.c_str()
+            );
+    fprintf(stderr, "Options\n"
+            "-bins <num of bins>, default: 2000\n"
+            "-binsize <size of bin>, default: 1.0 (km/s)\n");
+}
+
+
+
+
+int main(int argv, char * args[]){
+    GSnap * psnap;
+    
+    int k = 0;
+    if(argv < 4){
+        printUsage(args[0]);
+        exit(1);
+    }else if((argv == 4) || (args[4][0] == '-')){
+        singlefilename = args[1];
+        stringstream s0, s1;
+        s0 << args[2];
+        s0 >> radius;
+        s1 << args[3];
+        s1 >> boxSize;
+        
+        fprintf(stderr, "Fileame: %s\n", singlefilename.c_str());
+        k = 4;
+        
+    }else if(argv >= 6){
+        prefix = args[1];
+        basename_ = args[2];
+        numOfFiles = atoi(args[3]);
+        
+        fprintf(stderr, "Prefix: %s\n", prefix.c_str());
+        fprintf(stderr, "BaseName: %s\n", basename_.c_str());
+        fprintf(stderr, "Num of Files: %d\n", numOfFiles);
+        
+        stringstream s0, s1;
+        s0 << args[4];
+        s0 >> radius;
+        s1 << args[5];
+        s1 >> boxSize;
+        k = 6;
+    }else{
+        printUsage(args[0]);
+        exit(1);
+    }
+    
+    // fprintf(stderr, "%d %d\n", k, argv);
+    while(k < argv){
+        stringstream ss;
+        //fprintf(stderr, "%s\n", args[k]);
+        if(strcmp(args[k], "-bins") == 0){
+            ss << args[k + 1];
+            ss >> num_of_bins;
+        }else if(strcmp(args[k], "-binsize") == 0){
+            ss << args[k + 1];
+            ss >> bin_size;
+        }else{
+            printUsage(args[0]);
+            exit(1);
+        }
+        k += 2;
+    }
+    
+    fprintf(stderr, "radius = %f (kpc / h)\n", radius);
+    fprintf(stderr, "boxSize = %f (kpc / h)\n", boxSize);
+    fprintf(stderr, "numofbins = %d\n", num_of_bins);
+    fprintf(stderr, "bin_size = %f (km/s / h)\n", bin_size);
+    
+    max_w = num_of_bins / 2 * bin_size;
+    
+    z_w = new uint32_t[num_of_bins];
+    if(numOfFiles == 0 && singlefilename == ""){
+        printUsage(args[0]);
+        exit(1);
+    }
+    
+    for(int i = 0; i < num_of_bins; i++){
+        z_w[i] = 0;
+    }
+    
+    if(numOfFiles != 0){
+        //fprintf(stderr, "Create file!\n");
+        psnap = new GSnap(
+                          prefix,
+                          basename_,
+                          numOfFiles,
+                          true,
+                          1,
+                          -1);
+    }else{
+        psnap = new GSnap(singlefilename, true, 1, -1);
+    }
+    
+    allpos = psnap -> getAllPos();
+    allvel = psnap -> getAllVel();
+    size_t nparts = psnap -> Npart;
+    retVec.reserve(nparts);
+    
+    //z = 1/a - 1
+    //a = 1 / (z + 1)
+    double redshift = psnap->header.redshift;
+    double a = 1.0 / ( redshift + 1.0);
+    double sqa = sqrt(a);
+    
+    //convert velocity to km/s (peculiar velocity)
+    for(size_t i = 0; i < nparts; i++){
+        allvel[i] = allvel[i] * sqa;
+    }
+    
+    // make random 3d points
+    size_t m_count = nparts / 50;
+    fprintf(stderr, "Building tree ...\n");
+    for ( size_t n = 0; n < nparts; ++n)
+    {
+        
+        kdtreeNode node;
+        node.index = n;
+        kdtree.insert( node);
+        if(n % m_count == 0){
+            fprintf(stderr, ">");
+            cout.flush();
+        }
+    }
+    fprintf(stderr, "\n");
+    fprintf(stderr, "Optimising tree ...\n");
+    kdtree.optimise();
+    fprintf(stderr, "KDTree Built. Nparts: %ld\n", nparts);
+    
+    
+    
+    for( size_t i = 0; i < nparts; i ++){
+        if(i % m_count == 0){
+            fprintf(stderr, ">");
+            cout.flush();
+        }
+        Point vecP;
+        vecP.x = 0;
+        vecP.y = 0;
+        vecP.z = radius;
+        retVec.clear();
+        findPartsInBox(kdtree, allpos[i],
+                         vecP, boxSize/2,
+                         retVec);
+        
+        //printf("retsize = %ld, capacit = %ld, nparts = %ld\n",
+        //                retVec.size(), retVec.capacity(), nparts);
+
+        for(size_t j = 0; j < retVec.size(); j ++){
+            //printf("ok1 %d\n", i);
+            rvector = allpos[retVec[j].index] - allpos[i];
+            //printf("ok2 %d\n", j);
+            float r = sqrt(rvector.dot(rvector));
+            rvector = rvector / r;
+            if(r >= radius - boxSize/2 && r < radius + boxSize/2){
+                wvector = allvel[retVec[j].index] - allvel[i];
+                float w = wvector.dot(rvector);
+                
+                int ind = (int) floor(w / max_w * (num_of_bins / 2) + num_of_bins / 2);
+                //printf("%f %d\n", w);
+                if(ind >= 0 && ind < num_of_bins){
+                    z_w[ind] ++;
+                }
+            }
+        }
+    }
+    fprintf(stderr, "\n");
+    
+    for(int i = 0; i < num_of_bins; i++){
+        float w = (float)(i - num_of_bins / 2) / (float)(num_of_bins / 2) * max_w;
+        fprintf(stdout, "%f %d\n", w, z_w[i]);
+    }
+    delete z_w;
+    delete psnap;
+}
